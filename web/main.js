84ec84b4135b4a51fd2d65a9030184f040774c70
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2023-09-12 19:06:11 -0300

    Dir organization

--- web/main.js
+++ web/main.js
@@ -2,6 +2,6 @@ import "./styles/base.css"
 import "./styles/app.css"
 import "./styles/form.css"
 import "./styles/light-theme.css"
-import { toggleMode } from "./theme.js"
+import { toggleMode } from "./js/theme.js"
 
 window.toggleMode = toggleMode
